a20c538e1a8fa68867611849dde5da1d70595604
justin <>
2021-08-18 22:29:53 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
+      <center className="grid place-items-center mt-10">
+        <h1 className="text-5xl">Lets build Amazon 2.0</h1>
+        <h2>This is your starter template!</h2>
+        <br />
+        <h3 className="font-bold">
+          We will be using Next.js / Tailwind CSS / Redux / Firebase / NextAuth
+        </h3>
+
+      </center>
+      {/* ---- ---- */}
+    </div>
+  );
+}
